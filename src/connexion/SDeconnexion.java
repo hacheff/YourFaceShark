5984dd0f7caec3f145617ac94740cf2495cf59b4
hugo <hugo.fontaine@live.fr>
2013-03-21 16:21:31 +0100

    c'est un batard

---
+++ src/connexion/SDeconnexion.java
@@ -0,0 +1,52 @@
+package connexion;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SDeconnexion
+ */
+@WebServlet("/SDeconnexion")
+public class SDeconnexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SDeconnexion() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+request.getSession().setAttribute("user", null);
+		
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null){
+			for(int i = 0; i < cookies.length; i++){
+				if(SConnexion.COOKIE_NAME.equals(cookies[i].getName())){					
+					Cookie auChocolat = cookies[i];
+					auChocolat.setValue(null);
+					auChocolat.setMaxAge(0);
+					response.addCookie(auChocolat);
+				}
+			}
+		}
+		response.sendRedirect("jsp/connexion.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
