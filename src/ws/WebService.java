0d46eb8c09096dcf526dbc2fc0c78390b97d7153
hugo <hugo.fontaine@live.fr>
2013-04-22 10:13:05 +0200

    :p :D :)

--- src/ws/WebService.java
+++ src/ws/WebService.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import social.User;
+
+import bdd.Connexion;
+
 /**
  * Servlet implementation class WebService
  */
@@ -26,11 +30,37 @@ public class WebService extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// URL DE TEST: http://brain.fuck.free.fr/YFS/
 		String mail = request.getParameter("mail");
 		String mdp = request.getParameter("mdp");
 		String url = request.getParameter("url");
-		String xml = "<YFS>Test ok</YFS>";
+		StringBuffer xml = new StringBuffer();
+		xml.append("<YFS>");
 		
+		if(!Connexion.connectUser(mail, mdp, request.getSession())){
+			xml.append("<ERROR>connection db failed</ERROR>");
+		}
+		else{
+			User user = (User) request.getSession().getAttribute("user");
+
+			if(user != null){				
+				xml.append("<USER>");
+				xml.append("<ID>" + user.getId() + "</ID>");
+				xml.append("<SEXE>" + user.getSexe() + "</SEXE>");
+				xml.append("<NOM>" + user.getNom() + "</NOM>");
+				xml.append("<PRENOM>" + user.getPrenom() + "</PRENOM>");
+				xml.append("<MAIL>" + user.getMail() + "</MAIL>");
+				xml.append("<MDP>" + user.getMdp() + "</MDP>");
+				xml.append("<MDP>" + user.getMdp() + "</MDP>");
+				xml.append("<DATENAISSANCE>" + user.getDate() + "</DATENAISSANCE>");
+				xml.append("</USER>");
+			}
+			else {
+				xml.append("<ERROR>connection failed</ERROR>");
+			}
+		}
+		
+		xml.append("</YFS>");
 		
 		response.sendRedirect(url + "?yfs=true&xml="+xml);
 	}
